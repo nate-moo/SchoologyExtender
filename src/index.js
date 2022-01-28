7e56ed5428c6ab9597084e176c8bafc32173b489
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-01-28 11:22:08 -0500

    Adding playposit true fullscreen

--- src/index.js
+++ src/index.js
@@ -25,28 +25,46 @@ const onclick = async (Drupal) => {
 
 
 function main() {
-    let reloadAnchor = document.querySelector(".realm-top-right");
-    let reloadAnchor2 = document.querySelector("#toolbar-options-wrapper");
-    let reloadDivElement = document.createElement("div");
-    let reloadElement = document.createElement("a");
-    let reloadElementText = document.createElement("span");
-
-    if (!reloadAnchor) {
-        reloadAnchor = document.querySelector("#toolbar-options-wrapper");
+    if (document.querySelector(".realm-top-right") || document.querySelector("#toolbar-options-wrapper")) {
+        let reloadAnchor = document.querySelector(".realm-top-right");
+        let reloadAnchor2 = document.querySelector("#toolbar-options-wrapper");
+        let reloadDivElement = document.createElement("div");
+        let reloadElement = document.createElement("a");
+        let reloadElementText = document.createElement("span");
+    
+        if (!reloadAnchor) {
+            reloadAnchor = document.querySelector("#toolbar-options-wrapper");
+        }
+    
+        reloadElement.classList.add("link-btn");
+        reloadElement.setAttribute("id", "dingus")
+    
+        reloadDivElement.style = "float: left;";
+        
+        reloadElement.setAttribute("onclick", `(${onclick})(Drupal);`)
+        reloadElementText.innerText = "Reload";
+        reloadAnchor.style="max-width: 100%;";
+    
+        reloadElement.appendChild(reloadElementText);
+        reloadDivElement.appendChild(reloadElement);
+        reloadAnchor.appendChild(reloadDivElement);
     }
 
-    reloadElement.classList.add("link-btn");
-    reloadElement.setAttribute("id", "dingus")
+    console.log("checking for playposit button")
 
-    reloadDivElement.style = "float: left;";
-    
-    reloadElement.setAttribute("onclick", `(${onclick})(Drupal);`)
-    reloadElementText.innerText = "Reload";
-    reloadAnchor.style="max-width: 100%;";
-
-    reloadElement.appendChild(reloadElementText);
-    reloadDivElement.appendChild(reloadElement);
-    reloadAnchor.appendChild(reloadDivElement);
+    if (document.getElementsByClassName("fullscreen-option")[0]) {
+        let anchor = document.getElementsByClassName("fullscreen-option")[0];
+        anchor.children[0].remove();
+        let newButton = document.createElement("span");
+        let empty = document.createElement("span");
+        newButton.classList.add("link-btn");
+        newButton.classList.add("fullscreen");
+        //newButton.style.width = "10%";
+        //newButton.style.height = "10%";
+        newButton.appendChild(empty);
+        newButton.setAttribute("onclick", "document.getElementsByTagName('iframe')[0].requestFullscreen()");
+        anchor.appendChild(newButton);
+    }
 }
 
 main();
